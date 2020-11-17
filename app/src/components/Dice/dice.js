764381b39486ab70725ae98de701bccd0804f964
Chris Nyberg <chrisn83@gmail.com>
2020-11-17 12:08:42 -0500

    player one and player two created

--- app/src/components/Dice/dice.js
+++ app/src/components/Dice/dice.js
@@ -4,28 +4,53 @@ import { Container } from 'react-bootstrap';
 
 class Dice extends React.Component {
     state = {
-        diceSet: {
-            diceOne: ['axe', 'arrowEX', 'stealEX', 'helmet', 'shield', 'axe'],
-            diceTwo: ['ArrowEX', 'helmetEX', 'axe', 'axe', 'shield', 'steal'],
-            diceThree: ['shieldEX', 'stealEX', 'axe', 'axe', 'arrow', 'helmet'],
-            diceFour: ['shieldEX', 'helmetEX', 'axe', 'axe', 'steal', 'arrow'],
-            diceFive: ['stealEx', 'helmetEX', 'shield', 'arrow', 'axe', 'axe'],
-            diceSix: ['shieldEX', 'arrowEX', 'steal', 'helmet', 'axe', 'axe']
-        }
+        playerOne: {
+            diceSet: {
+                diceOne: ['axe', 'arrowEX', 'stealEX', 'helmet', 'shield', 'axe'],
+                diceTwo: ['ArrowEX', 'helmetEX', 'axe', 'axe', 'shield', 'steal'],
+                diceThree: ['shieldEX', 'stealEX', 'axe', 'axe', 'arrow', 'helmet'],
+                diceFour: ['shieldEX', 'helmetEX', 'axe', 'axe', 'steal', 'arrow'],
+                diceFive: ['stealEx', 'helmetEX', 'shield', 'arrow', 'axe', 'axe'],
+                diceSix: ['shieldEX', 'arrowEX', 'steal', 'helmet', 'axe', 'axe']
+            },
+            decidingCup: []
+        },
+        playerTwo: {
+            diceSet: {
+                diceOne: ['axe', 'arrowEX', 'stealEX', 'helmet', 'shield', 'axe'],
+                diceTwo: ['ArrowEX', 'helmetEX', 'axe', 'axe', 'shield', 'steal'],
+                diceThree: ['shieldEX', 'stealEX', 'axe', 'axe', 'arrow', 'helmet'],
+                diceFour: ['shieldEX', 'helmetEX', 'axe', 'axe', 'steal', 'arrow'],
+                diceFive: ['stealEx', 'helmetEX', 'shield', 'arrow', 'axe', 'axe'],
+                diceSix: ['shieldEX', 'arrowEX', 'steal', 'helmet', 'axe', 'axe']
+            },
+            decidingCup: []
+        },
     };
-    rollDice = e => {
+    rollDicePOne = e => {
         //    return 
-        console.log("DICE ONE: " + this.state.diceSet.diceOne[Math.floor(Math.random() * this.state.diceSet.diceOne.length)])
-        console.log("DICE TWO: " + this.state.diceSet.diceTwo[Math.floor(Math.random() * this.state.diceSet.diceTwo.length)])
-        console.log("DICE Three: " + this.state.diceSet.diceThree[Math.floor(Math.random() * this.state.diceSet.diceThree.length)])
-        console.log("DICE Four: " + this.state.diceSet.diceFour[Math.floor(Math.random() * this.state.diceSet.diceFour.length)])
-        console.log("DICE Five: " + this.state.diceSet.diceFive[Math.floor(Math.random() * this.state.diceSet.diceFive.length)])
-        console.log("DICE Six: " + this.state.diceSet.diceSix[Math.floor(Math.random() * this.state.diceSet.diceSix.length)])
+        console.log("P1 DICE ONE: " + this.state.playerOne.diceSet.diceOne[Math.floor(Math.random() * this.state.playerOne.diceSet.diceOne.length)])
+        console.log("P1 DICE TWO: " + this.state.playerOne.diceSet.diceTwo[Math.floor(Math.random() * this.state.playerOne.diceSet.diceTwo.length)])
+        console.log("P1 DICE Three: " + this.state.playerOne.diceSet.diceThree[Math.floor(Math.random() * this.state.playerOne.diceSet.diceThree.length)])
+        console.log("P1 DICE Four: " + this.state.playerOne.diceSet.diceFour[Math.floor(Math.random() * this.state.playerOne.diceSet.diceFour.length)])
+        console.log("P1 DICE Five: " + this.state.playerOne.diceSet.diceFive[Math.floor(Math.random() * this.state.playerOne.diceSet.diceFive.length)])
+        console.log("P1 DICE Six: " + this.state.playerOne.diceSet.diceSix[Math.floor(Math.random() * this.state.playerOne.diceSet.diceSix.length)])
+    };
+    rollDicePTwo = e => {
+        //    return 
+        console.log("P2 DICE ONE: " + this.state.playerOne.diceSet.diceOne[Math.floor(Math.random() * this.state.playerOne.diceSet.diceOne.length)])
+        console.log("P2 DICE TWO: " + this.state.playerOne.diceSet.diceTwo[Math.floor(Math.random() * this.state.playerOne.diceSet.diceTwo.length)])
+        console.log("P2 DICE Three: " + this.state.playerOne.diceSet.diceThree[Math.floor(Math.random() * this.state.playerOne.diceSet.diceThree.length)])
+        console.log("P2 DICE Four: " + this.state.playerOne.diceSet.diceFour[Math.floor(Math.random() * this.state.playerOne.diceSet.diceFour.length)])
+        console.log("P2 DICE Five: " + this.state.playerOne.diceSet.diceFive[Math.floor(Math.random() * this.state.playerOne.diceSet.diceFive.length)])
+        console.log("P2 DICE Six: " + this.state.playerOne.diceSet.diceSix[Math.floor(Math.random() * this.state.playerOne.diceSet.diceSix.length)])
     };
     render() {
         return (
             <Container>
-                <button onClick={this.rollDice}>CLICK TO ROLL</button>
+                <button onClick={this.rollDicePOne}>CLICK TO ROLL Player One</button>
+                <br/>
+                <button onClick={this.rollDicePTwo}>CLICK TO ROLL Player Two</button>
             </Container>
         );
     };
