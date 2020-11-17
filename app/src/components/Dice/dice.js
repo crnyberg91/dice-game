1e75945a818a67340fa10d5ffda6fd967797693a
Chris Nyberg <chrisn83@gmail.com>
2020-11-17 12:22:20 -0500

    correction to Player two

--- app/src/components/Dice/dice.js
+++ app/src/components/Dice/dice.js
@@ -25,7 +25,7 @@ class Dice extends React.Component {
                 diceSix: ['shieldEX', 'arrowEX', 'steal', 'helmet', 'axe', 'axe']
             },
             decidingCup: []
-        },
+        }
     };
     rollDicePOne = e => {
         //    return 
@@ -38,13 +38,14 @@ class Dice extends React.Component {
     };
     rollDicePTwo = e => {
         //    return 
-        console.log("P2 DICE ONE: " + this.state.playerOne.diceSet.diceOne[Math.floor(Math.random() * this.state.playerOne.diceSet.diceOne.length)])
-        console.log("P2 DICE TWO: " + this.state.playerOne.diceSet.diceTwo[Math.floor(Math.random() * this.state.playerOne.diceSet.diceTwo.length)])
-        console.log("P2 DICE Three: " + this.state.playerOne.diceSet.diceThree[Math.floor(Math.random() * this.state.playerOne.diceSet.diceThree.length)])
-        console.log("P2 DICE Four: " + this.state.playerOne.diceSet.diceFour[Math.floor(Math.random() * this.state.playerOne.diceSet.diceFour.length)])
-        console.log("P2 DICE Five: " + this.state.playerOne.diceSet.diceFive[Math.floor(Math.random() * this.state.playerOne.diceSet.diceFive.length)])
-        console.log("P2 DICE Six: " + this.state.playerOne.diceSet.diceSix[Math.floor(Math.random() * this.state.playerOne.diceSet.diceSix.length)])
+        console.log("P2 DICE ONE: " + this.state.playerTwo.diceSet.diceOne[Math.floor(Math.random() * this.state.playerTwo.diceSet.diceOne.length)])
+        console.log("P2 DICE TWO: " + this.state.playerTwo.diceSet.diceTwo[Math.floor(Math.random() * this.state.playerTwo.diceSet.diceTwo.length)])
+        console.log("P2 DICE Three: " + this.state.playerTwo.diceSet.diceThree[Math.floor(Math.random() * this.state.playerTwo.diceSet.diceThree.length)])
+        console.log("P2 DICE Four: " + this.state.playerTwo.diceSet.diceFour[Math.floor(Math.random() * this.state.playerTwo.diceSet.diceFour.length)])
+        console.log("P2 DICE Five: " + this.state.playerTwo.diceSet.diceFive[Math.floor(Math.random() * this.state.playerTwo.diceSet.diceFive.length)])
+        console.log("P2 DICE Six: " + this.state.playerTwo.diceSet.diceSix[Math.floor(Math.random() * this.state.playerTwo.diceSet.diceSix.length)])
     };
+    
     render() {
         return (
             <Container>
