7eb572ddf7e184fe09ebc2f68564f00831017e05
Chris Nyberg <chrisn83@gmail.com>
2020-11-24 17:52:30 -0500

    basic-styling, containers created, font-family aded to html

--- app/src/components/DiceGame/diceGame.js
+++ app/src/components/DiceGame/diceGame.js
@@ -11,15 +11,15 @@ class DiceGame extends React.Component {
         },
         playerOne: {
             diceSet: {
-                diceOne: ['axe', 'arrowEX', 'stealEX', 'helmet', 'shield', 'axe'],
-                diceTwo: ['ArrowEX', 'helmetEX', 'axe', 'axe', 'shield', 'steal'],
-                diceThree: ['shieldEX', 'stealEX', 'axe', 'axe', 'arrow', 'helmet'],
-                diceFour: ['shieldEX', 'helmetEX', 'axe', 'axe', 'steal', 'arrow'],
+                diceOne: ['arrowEX', 'stealEX', 'helmet', 'shield', 'axe', 'axe'],
+                diceTwo: ['arrowEX', 'helmetEX', 'shield', 'steal', 'axe', 'axe'],
+                diceThree: ['shieldEX', 'stealEX', 'arrow', 'helmet', 'axe', 'axe'],
+                diceFour: ['shieldEX', 'helmetEX', 'steal', 'arrow', 'axe', 'axe'],
                 diceFive: ['stealEx', 'helmetEX', 'shield', 'arrow', 'axe', 'axe'],
                 diceSix: ['shieldEX', 'arrowEX', 'steal', 'helmet', 'axe', 'axe']
             },
             decidingCup: [],
-            healthPoints: 0,
+            healthPoints: 15,
             selectedPowers: [],
             isFirst: false
         },
@@ -33,7 +33,7 @@ class DiceGame extends React.Component {
                 diceSix: ['shieldEX', 'arrowEX', 'steal', 'helmet', 'axe', 'axe']
             },
             decidingCup: [],
-            healthPoints: 0,
+            healthPoints: 15,
             selectedPowers: [],
             isFirst: false
         }
@@ -56,39 +56,83 @@ class DiceGame extends React.Component {
         console.log("P2 DICE Five: " + this.state.playerTwo.diceSet.diceFive[Math.floor(Math.random() * this.state.playerTwo.diceSet.diceFive.length)])
         console.log("P2 DICE Six: " + this.state.playerTwo.diceSet.diceSix[Math.floor(Math.random() * this.state.playerTwo.diceSet.diceSix.length)])
     };
+    //coin flip that creates coin div in which side that won
+    //selected dice will move to the selDiceCup
+    //Sorting algorithm used to create the attack phase row
+    //rounds changing who goes first
 
     render() {
         return (
             <Container className='dice-game'>
-                <div className='opponent-side-powers'>opponent powers</div>
+                <div className='opponent-side-powers'>
+                    <div className='power' id='oppPower1'>Power1</div>
+                    <div className='power' id='oppPower2'>Power2</div>
+                    <div className='power' id='oppPower3'>Power3</div>
+                </div>
                 <div className='opponent-side-cup'>
-                    oppCup
+                    <div className='dice' id='oppDice1'>dice1</div>
+                    <div className='dice' id='oppDice2'>dice2</div>
+                    <div className='dice' id='oppDice3'>dice3</div>
+                    <div className='dice' id='oppDice4'>dice4</div>
+                    <div className='dice' id='oppDice5'>dice5</div>
+                    <div className='dice' id='oppDice6'>dice6</div>
                 </div>
                 <div className='opponent-side-health-points'>
-                    oppHP
+                    <div className='health-pool' id='oppHealthPool'>{this.state.playerOne.healthPoints}</div>
                 </div>
                 <div className='empty-space'>emptyspace</div>
-                <div className='opponent-roll-btn'>opponent rollbutton</div>
+                <div className='opponent-roll-btn'>
+                    <button className='roll-btn'>opponent roll button</button>
+                </div>
                 <div className='empty-space2'>empty-space2</div>
-                <div className='opponent-meter'>opponent meter</div>
-                <div className='opponent-selected-dice-cup'>opponent selected Dice</div>
+                <div className='opponent-meter'>
+                    <div className='meter-bar' id='oppMeterBar'>oppMeter</div>
+                </div>
+                <div className='opponent-selected-dice-cup'>
+                    <div className='dice' id='oppSelDice1'>dice1</div>
+                    <div className='dice' id='oppSelDice2'>dice2</div>
+                    <div className='dice' id='oppSelDice3'>dice3</div>
+                    <div className='dice' id='oppSelDice4'>dice4</div>
+                    <div className='dice' id='oppSelDice5'>dice5</div>
+                    <div className='dice' id='oppSelDice6'>dice6</div>
+                </div>
                 <div className='empty-space3'>emptyspace3</div>
-                <div className='opponent-coin'>opponent coin</div>
-                <div className='empty-space4'>emptySpace4</div>
-                <div className='opponent-attack-row'>opponent attack row</div>
-                <div className='player-attack-row'>player attack row</div>
-                <div className='empty-space5'>emptyspace5</div>
-                <div className='player-selected-dice'>player selected dice</div>
+                <div className='coin' id='oppCoin'>opponent coin</div>
+                {/* <div className='empty-space4'>emptySpace4</div> */}
+                <div className='attack-row' id='oppAtkRow'>opponent attack row</div>
+                <div className='attack-row' id='playerAtkRow'>player attack row</div>
+                {/* <div className='empty-space5'>emptyspace5</div> */}
+                <div className='player-selected-dice'>
+                    <div className='dice' id='playerSelDice1'>dice1</div>
+                    <div className='dice' id='playerSelDice2'>dice2</div>
+                    <div className='dice' id='playerSelDice3'>dice3</div>
+                    <div className='dice' id='playerSelDice4'>dice4</div>
+                    <div className='dice' id='playerSelDice5'>dice5</div>
+                    <div className='dice' id='playerSelDice6'>dice6</div>
+                </div>
                 <div className='empty-space6'>empty-space6</div>
-                <div className='player-coin'>player coin</div>
+                <div className='coin' id='playerCoin'>player coin</div>
                 <div className='empty-space7'>empty space7</div>
-                <div className='player-roll-btn'>player rollbutton</div>
+                <div className='player-roll-btn'><button className='roll-btn'>player roll button</button></div>
                 <div className='empty-space8'>empty space8</div>
-                <div className='player-meter'>player meter</div>
+                <div className='player-meter'><div className='meter-bar' id='oppMeterBar'>oppMeter</div></div>
                 {/* <div className='empty-space9'>empty space9</div> */}
-                <div className='player-side-health-points'>player HP</div>
-                <div className='player-side-cup'>player side cup</div>
-                <div className='player-side-powers'>powers</div>
+                <div className='player-side-health-points'>
+                    <div className='health-pool' id='playerHealthPool'>{this.state.playerTwo.healthPoints}</div>
+                </div>
+                <div className='player-side-cup'>
+                    <div className='dice' id='playerDice1'>dice1</div>
+                    <div className='dice' id='playerDice2'>dice2</div>
+                    <div className='dice' id='playerDice3'>dice3</div>
+                    <div className='dice' id='playerDice4'>dice4</div>
+                    <div className='dice' id='playerDice5'>dice5</div>
+                    <div className='dice' id='playerDice6'>dice6</div>
+                </div>
+                <div className='player-side-powers'>
+                    <div className='power' id='playerPower1'>Power1</div>
+                    <div className='power' id='playerPower2'>Power2</div>
+                    <div className='power' id='playerPower3'>Power3</div>
+                </div>
                 {/* <div className='player-side'>
                 <div className='health-points'>
                     Health points
